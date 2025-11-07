6eba420f05361b4595e73fe494243c1c4d3d1296
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2025-11-07 21:16:54 +0000

    feat: update example with some rules

--- ppeg-example/arithmetic-expression/src/main.rs
+++ ppeg-example/arithmetic-expression/src/main.rs
@@ -1,8 +1,14 @@
-use ppeg_core::parser::{Grammar, Parser};
+use ppeg_core::parser::{Grammar, Parser, Rule};
 
 fn main() {
-  let grammar = Grammar::new();
-  let parser = Parser::new("1+2*3", 0, grammar);
+  let mut grammar = Grammar::new();
+  grammar.insert(Rule::new("1", ppeg_core::parser::Expression::OneOrMore));
+  grammar.insert(Rule::new("2", ppeg_core::parser::Expression::OneOrMore));
+  grammar.insert(Rule::new("3", ppeg_core::parser::Expression::OneOrMore));
+  grammar.insert(Rule::new("*", ppeg_core::parser::Expression::OneOrOne));
+  grammar.insert(Rule::new("+", ppeg_core::parser::Expression::OneOrOne));
+
+  let mut parser = Parser::new("1+2*3", 0, grammar);
   let cst = parser.parse();
 
   match cst {
