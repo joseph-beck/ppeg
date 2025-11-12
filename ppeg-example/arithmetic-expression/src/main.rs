572a626e96656cd5f9fd6808b9df17db55f2e0f8
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2025-11-12 18:25:15 +0000

    feat: overhaul named rules to allow multiple values for better parsing of choice

--- ppeg-example/arithmetic-expression/src/main.rs
+++ ppeg-example/arithmetic-expression/src/main.rs
@@ -2,14 +2,16 @@ use ppeg_core::parser::{Expression, Grammar, Parser, Rule};
 
 fn main() {
   let mut grammar = Grammar::new();
-  grammar.insert(Rule::new("WHITESPACE", " ", Expression::Empty));
-  grammar.insert(Rule::new("ONE", "1", Expression::OneOrMore));
-  grammar.insert(Rule::new("TWO", "2", Expression::OneOrMore));
-  grammar.insert(Rule::new("THREE", "3", Expression::OneOrMore));
-  grammar.insert(Rule::new("L_BRACKET", "(", Expression::OneOrOne));
-  grammar.insert(Rule::new("R_BRACKET", ")", Expression::OneOrOne));
-  grammar.insert(Rule::new("STAR", "*", Expression::OneOrOne));
-  grammar.insert(Rule::new("PLUS", "+", Expression::OneOrOne));
+  grammar.insert(Rule::new("WHITESPACE", vec![" "], Expression::Empty));
+  grammar.insert(Rule::new(
+    "DIGIT",
+    vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    Expression::OneOrMore,
+  ));
+  grammar.insert(Rule::new("L_BRACKET", vec!["("], Expression::Choice));
+  grammar.insert(Rule::new("R_BRACKET", vec![")"], Expression::Choice));
+  grammar.insert(Rule::new("STAR", vec!["*"], Expression::Choice));
+  grammar.insert(Rule::new("PLUS", vec!["+"], Expression::Choice));
 
   let mut parser = Parser::new("1+(2*3)", 0, grammar);
   let cst = parser.parse();
