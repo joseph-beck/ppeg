d1edb2cd4779927f992881178db99ef21c765308
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2025-12-21 16:55:32 +0000

    feat: add copy to clipboard functionality with tooltip

--- ppeg-ui/src/components/parser/parser-input-copy.tsx
+++ ppeg-ui/src/components/parser/parser-input-copy.tsx
@@ -1,12 +1,39 @@
 import { InputGroupButton } from '@shadcn/input-group'
-import { IconCopy } from '@tabler/icons-react'
-import { ReactElement } from 'react'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@shadcn/tooltip'
+import { P } from '@shadcn/typography'
+import { IconCheck, IconCopy } from '@tabler/icons-react'
+import { ReactElement, useState } from 'react'
+
+import { useParserForm } from './use-parser-form'
 
 const ParserInputCopy = (): ReactElement => {
+  const form = useParserForm()
+
+  const [copied, setCopied] = useState<boolean>(false)
+
   return (
-    <InputGroupButton className="ml-auto" size="icon-xs">
-      <IconCopy />
-    </InputGroupButton>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <InputGroupButton
+          className="ml-auto"
+          size="icon-xs"
+          onClick={() => {
+            navigator.clipboard.writeText(form.getFieldValue('input'))
+
+            setCopied(true)
+
+            setTimeout(() => {
+              setCopied(false)
+            }, 1000)
+          }}
+        >
+          {copied ? <IconCheck /> : <IconCopy />}
+        </InputGroupButton>
+      </TooltipTrigger>
+      <TooltipContent>
+        <P>Copy to Clipboard</P>
+      </TooltipContent>
+    </Tooltip>
   )
 }
 
