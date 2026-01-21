af0d1c228b85456b0fa8639271d17c1282ea94f4
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-01-21 11:45:46 +0000

    feat: update presets and add arithmetic expression rule

--- ppeg-ui/src/components/parser/get-presets.ts
+++ ppeg-ui/src/components/parser/get-presets.ts
@@ -1,5 +1,7 @@
-import { parserInputRuleCharMock } from '@/mocks/parser-input-rule-char-mock'
-import { parserInputRuleNumberMock } from '@/mocks/parser-input-rule-number-mock'
+import { parserInputRuleArithmeticExpressionMock } from '@/mocks/parser-input-rule-arithmetic-expression.mock'
+import { parserInputRuleCharMock } from '@/mocks/parser-input-rule-char.mock'
+import { parserInputRuleNumberMock } from '@/mocks/parser-input-rule-number.mock'
+import { parserInputRuleWordMock } from '@/mocks/parser-input-rule-word.mock'
 import { Parse } from '@/types/ppeg/parse'
 
 interface GetPresetsResult {
@@ -19,6 +21,16 @@ const getPresets = (): GetPresetsResult[] => {
       label: 'number rule',
       description: 'grammar for parsing numbers, such as "123"',
       parse: parserInputRuleNumberMock,
+    },
+    {
+      label: 'word rule',
+      description: 'grammar for parsing a word',
+      parse: parserInputRuleWordMock,
+    },
+    {
+      label: 'arithmetic expression rule',
+      description: 'grammar for parsing arithmetic expressions, such as "1+(2*3)"',
+      parse: parserInputRuleArithmeticExpressionMock,
     },
   ]
 }
