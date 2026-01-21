9e889aab715f0e9666b7f95967fdde39b6b59832
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-01-21 21:15:42 +0000

    feat: update packrat memo table

--- ppeg-core/src/packrat.rs
+++ ppeg-core/src/packrat.rs
@@ -1,26 +1,76 @@
 use std::collections::HashMap;
 
-use crate::parser::Output;
+use crate::{cst::CST, error::ParserError};
+
+/// When parsing a rule using Packrat parsing state must be tracked.
+/// This enum represents the possible states of parsing a rule.
+#[derive(Debug, Clone, PartialEq)]
+pub enum State<'a> {
+  /// The parser is currently seeding this rule.
+  Seeding,
+  /// The rule has been successfully parsed.
+  Parsed(&'a str, Option<CST<'a>>),
+  /// The rule failed to parse.
+  Failed(ParserError<'a>),
+}
 
 /// Packrat memoization table for storing previous parser state.
+#[derive(Clone, Debug, PartialEq)]
 pub struct Packrat<'a> {
   /// Memoization table mapping (grammar rule, input position) to parser output.
-  memo_table: HashMap<(&'a str, usize), Output<'a>>,
+  memo_table: HashMap<(&'a str, &'a str), Result<State<'a>, ParserError<'a>>>,
+  /// Seeding flag for left recursion handling.
+  seeding: bool,
 }
 
 impl<'a> Packrat<'a> {
+  /// Create a new empty Packrat memoization table.
+  /// HashMap is defaulted to a new empty map.
+  /// Seeding flag is defaulted to false.
   pub fn new() -> Self {
     Packrat {
       memo_table: HashMap::new(),
+      seeding: false,
     }
   }
 
-  pub fn insert(&mut self, key: (&'a str, usize), output: Output<'a>) {
+  /// Get a memoized result for a given (rule, input) pair.
+  pub fn get(&self, key: (&'a str, &'a str)) -> Option<&Result<State<'a>, ParserError<'a>>> {
+    self.memo_table.get(&key)
+  }
+
+  /// Insert a memoized result for a given (rule, input) pair.
+  pub fn insert(&mut self, key: (&'a str, &'a str), output: Result<State<'a>, ParserError<'a>>) {
     self.memo_table.insert(key, output);
   }
 
-  pub fn get(&self, key: (&'a str, usize)) -> Option<&Output<'a>> {
-    self.memo_table.get(&key)
+  /// Remove a memoized result for a given (rule, input) pair.
+  pub fn remove(&mut self, key: (&'a str, &'a str)) {
+    self.memo_table.remove(&key);
+  }
+
+  /// Mark a rule at a given input position as being in the seeding state.
+  pub fn mark(&mut self, key: (&'a str, &'a str)) {
+    self.memo_table.insert(key, Ok(State::Seeding));
+  }
+
+  /// Update the memoization table only if the current state is Seeding.
+  pub fn update_when_seeding(&mut self, key: (&'a str, &'a str), result: Result<State<'a>, ParserError<'a>>) {
+    let data = self.get(key);
+
+    if let Some(Ok(State::Seeding)) = data {
+      self.memo_table.insert(key, result);
+    }
+  }
+
+  /// Set the seeding flag.
+  pub fn set_seeding(&mut self, seeding: bool) {
+    self.seeding = seeding;
+  }
+
+  /// Check if currently seeding
+  pub fn is_seeding(&self) -> bool {
+    self.seeding
   }
 }
 
@@ -34,8 +84,6 @@ impl<'a> Default for Packrat<'a> {
 mod tests {
   use super::*;
 
-  use crate::parser::Expression;
-
   #[test]
   fn test_packrat_new() {
     let packrat: Packrat = Packrat::new();
@@ -46,28 +94,5 @@ mod tests {
   fn test_packrat_default() {
     let packrat: Packrat = Packrat::default();
     assert!(packrat.memo_table.is_empty());
-  }
-
-  #[test]
-  fn test_packrat_insert() {
-    let mut packrat: Packrat = Packrat::new();
-    let key = ("rule1", 0);
-    let output = Output::new(Ok(vec!["a", "b"]), Expression::Empty);
-
-    packrat.insert(key, output);
-
-    assert!(packrat.memo_table.get(&key).is_some());
-    assert!(packrat.memo_table.get(&("rule1", 1)).is_none());
-  }
-
-  #[test]
-  fn test_packrat_get() {
-    let mut packrat: Packrat = Packrat::new();
-    let key = ("rule1", 0);
-    let output = Output::new(Ok(vec!["a", "b"]), Expression::Empty);
-    packrat.memo_table.insert(key, output);
-
-    assert!(packrat.get(key).is_some());
-    assert!(packrat.get(("rule1", 1)).is_none());
   }
 }
