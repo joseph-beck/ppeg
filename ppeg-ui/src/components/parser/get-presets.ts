86b2a75346827c4e2c67014ae84e5b5c16602c1c
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-01-22 08:37:43 +0000

    feat: add direct and indirect lr rules to presets in ppeg-ui

--- ppeg-ui/src/components/parser/get-presets.ts
+++ ppeg-ui/src/components/parser/get-presets.ts
@@ -1,5 +1,7 @@
 import { parserInputRuleArithmeticExpressionMock } from '@/mocks/parser-input-rule-arithmetic-expression.mock'
 import { parserInputRuleCharMock } from '@/mocks/parser-input-rule-char.mock'
+import { parserInputRuleDirectLeftRecursion } from '@/mocks/parser-input-rule-direct-left-recursion'
+import { parserInputRuleIndirectLeftRecursion } from '@/mocks/parser-input-rule-indirect-left-recursion'
 import { parserInputRuleNumberMock } from '@/mocks/parser-input-rule-number.mock'
 import { parserInputRuleWordMock } from '@/mocks/parser-input-rule-word.mock'
 import { Parse } from '@/types/ppeg/parse'
@@ -31,6 +33,16 @@ const getPresets = (): GetPresetsResult[] => {
       label: 'arithmetic expression rule',
       description: 'grammar for parsing arithmetic expressions, such as "1+(2*3)"',
       parse: parserInputRuleArithmeticExpressionMock,
+    },
+    {
+      label: 'indirect left recursion rule',
+      description: 'grammar demonstrating indirect left recursion',
+      parse: parserInputRuleDirectLeftRecursion,
+    },
+    {
+      label: 'direct left recursion rule',
+      description: 'grammar demonstrating direct left recursion',
+      parse: parserInputRuleIndirectLeftRecursion,
     },
   ]
 }
---
+++ ppeg-ui/src/mocks/parser-input-rule-direct-left-recursion.ts
@@ -0,0 +1,68 @@
+import { Parse } from '@/types/ppeg/parse'
+
+const parserInputRuleDirectLeftRecursion: Parse = {
+  grammar: {
+    rules: [
+      {
+        name: 'rule_number',
+        expression: {
+          type: 'Choice',
+          value: [
+            {
+              type: 'Char',
+              value: '1',
+            },
+            {
+              type: 'Char',
+              value: '2',
+            },
+            {
+              type: 'Char',
+              value: '3',
+            },
+          ],
+        },
+      },
+      {
+        name: 'rule_x',
+        expression: {
+          type: 'NamedRule',
+          value: 'rule_expr',
+        },
+      },
+      {
+        name: 'rule_expr',
+        expression: {
+          type: 'Choice',
+          value: [
+            {
+              type: 'Sequence',
+              value: [
+                {
+                  type: 'NamedRule',
+                  value: 'rule_x',
+                },
+                {
+                  type: 'Char',
+                  value: '+',
+                },
+                {
+                  type: 'NamedRule',
+                  value: 'rule_number',
+                },
+              ],
+            },
+            {
+              type: 'NamedRule',
+              value: 'rule_number',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  input: '1+1',
+  rule: 'rule_expr',
+}
+
+export { parserInputRuleDirectLeftRecursion }
