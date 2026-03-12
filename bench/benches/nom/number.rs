7bbcefa5ae0ab87eac460474a62497eb2cb83f45
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-03-12 10:30:00 +0000

    chore: update benches

--- bench/benches/nom/number.rs
+++ bench/benches/nom/number.rs
@@ -1,11 +1,16 @@
 use criterion::{Criterion, criterion_group};
+use nom::{IResult, character::complete::digit1};
 
-fn number() {
-  let _ = 1;
+fn number(input: &str) -> IResult<&str, &str> {
+  digit1(input)
+}
+
+fn parse_number() {
+  let _ = number("100000000");
 }
 
 fn bench_number(c: &mut Criterion) {
-  c.bench_function("nom_number", |b| b.iter(|| number()));
+  c.bench_function("nom_number", |b| b.iter(|| parse_number()));
 }
 
 criterion_group!(number_group, bench_number);
