4177426d1bd83facfe2053aa0cc6a3a3014b6006
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-03-29 17:35:32 +0100

    fix: build issue

--- ppeg-ui/src/routes/docs.tsx
+++ ppeg-ui/src/routes/docs.tsx
@@ -1,11 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { add } from 'ppeg-wasm'
 
 import { PPEGTitle } from '@/components/shared/ppeg-title'
 
 const Page = () => {
-  console.log(add(2, 2))
-
   return (
     <div className="p-2 flex flex-col items-center ">
       <PPEGTitle />
