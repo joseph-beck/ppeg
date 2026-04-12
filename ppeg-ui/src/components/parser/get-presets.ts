dfe41b7a0cca3b88be9bdc21a02b9e947253a87c
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-04-12 16:08:03 +0100

    chore: fix broken presets

--- ppeg-ui/src/components/parser/get-presets.ts
+++ ppeg-ui/src/components/parser/get-presets.ts
@@ -13,64 +13,69 @@ interface GetPresetsResult {
 const getPresets = (): GetPresetsResult[] => {
   return [
     {
-      label: 'character rule',
+      label: 'character',
       description: 'grammar for parsing the character "a"',
       parse: {
-        grammar: "rule_char := { 'a' }",
+        grammar: ["rule_char := { 'a' }", ''].join('\n'),
         input: 'a',
         rule: 'rule_char',
       },
     },
     {
-      label: 'number rule',
+      label: 'number',
       description: 'grammar for parsing numbers, such as "123"',
       parse: {
-        grammar: "rule_number := { { '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' }+ }",
+        grammar: ["rule_number := { { '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' }+ }", ''].join('\n'),
         input: '123',
         rule: 'rule_number',
       },
     },
     {
-      label: 'word rule',
+      label: 'word',
       description: 'grammar for parsing a word',
       parse: {
-        grammar:
+        grammar: [
           "rule_word := { { 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' }+ }",
+          '',
+        ].join('\n'),
         input: 'hello',
         rule: 'rule_word',
       },
     },
     {
-      label: 'arithmetic expression rule',
+      label: 'arithmetic expression',
       description: 'grammar for parsing arithmetic expressions, such as "1+2+3"',
       parse: {
-        grammar: ["rule_num := { { '1' | '2' | '3' }+ }", "rule_expr := { rule_expr, '+', rule_num | rule_num }"].join(
+        grammar: ["rule_num := { { '1' | '2' }+ }", "rule_expr := { rule_expr, '+', rule_num | rule_num }", ''].join(
           '\n',
         ),
-        input: '1+2+3',
+        input: '1+2',
         rule: 'rule_expr',
       },
     },
     {
-      label: 'indirect left recursion rule',
-      description: 'grammar demonstrating indirect left recursion',
+      label: 'direct left recursion',
+      description: 'grammar demonstrating direct left recursion',
       parse: {
         grammar: [
           "rule_num := { { '1' | '2' | '3' }+ }",
-          'rule_x := { rule_expr }',
-          "rule_expr := { rule_x, '+', rule_num | rule_num }",
+          "rule_expr := { rule_expr, '+', rule_expr | rule_num }",
+          '',
         ].join('\n'),
         input: '1+2+3',
         rule: 'rule_expr',
       },
     },
     {
-      label: 'direct left recursion rule',
-      description: 'grammar demonstrating direct left recursion',
+      label: 'indirect left recursion',
+      description: 'grammar demonstrating indirect left recursion',
       parse: {
-        grammar: ["rule_num := { { '1' | '2' | '3' }+ }", "rule_expr := { rule_expr, '+', rule_num | rule_num }"].join(
-          '\n',
-        ),
+        grammar: [
+          "rule_num := { { '1' | '2' | '3' }+ }",
+          'rule_x := { rule_expr }',
+          "rule_expr := { rule_x, '+', rule_x | rule_num }",
+          '',
+        ].join('\n'),
         input: '1+2+3',
         rule: 'rule_expr',
       },
