5831fe219b28c3e7362ba37c523cf284d465a6b2
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-04-13 18:45:29 +0100

    feat: new presets and fix old presets

--- ppeg-ui/src/components/parser/get-presets.ts
+++ ppeg-ui/src/components/parser/get-presets.ts
@@ -14,7 +14,7 @@ const getPresets = (): GetPresetsResult[] => {
   return [
     {
       label: 'character',
-      description: 'grammar for parsing the character "a"',
+      description: 'grammar for parsing the character "a".',
       parse: {
         grammar: ["rule_char := { 'a' }", ''].join('\n'),
         input: 'a',
@@ -23,7 +23,7 @@ const getPresets = (): GetPresetsResult[] => {
     },
     {
       label: 'number',
-      description: 'grammar for parsing numbers, such as "123"',
+      description: 'grammar for parsing numbers, such as "123".',
       parse: {
         grammar: ["rule_number := { { '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' }+ }", ''].join('\n'),
         input: '123',
@@ -32,7 +32,7 @@ const getPresets = (): GetPresetsResult[] => {
     },
     {
       label: 'word',
-      description: 'grammar for parsing a word',
+      description: 'grammar for parsing a word.',
       parse: {
         grammar: [
           "rule_word := { { 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' }+ }",
@@ -44,7 +44,7 @@ const getPresets = (): GetPresetsResult[] => {
     },
     {
       label: 'arithmetic expression',
-      description: 'grammar for parsing arithmetic expressions, such as "1+2+3"',
+      description: 'grammar for parsing arithmetic expressions, such as "1+2".',
       parse: {
         grammar: ["rule_num := { { '1' | '2' }+ }", "rule_expr := { rule_expr, '+', rule_num | rule_num }", ''].join(
           '\n',
@@ -54,30 +54,56 @@ const getPresets = (): GetPresetsResult[] => {
       },
     },
     {
-      label: 'direct left recursion',
-      description: 'grammar demonstrating direct left recursion',
+      label: 'arithmetic expression with left recursion',
+      description: 'grammar for parsing arithmetic expressions, such as "1+2+3", using left recursion.',
       parse: {
         grammar: [
-          "rule_num := { { '1' | '2' | '3' }+ }",
-          "rule_expr := { rule_expr, '+', rule_expr | rule_num }",
+          "n := { { '1' | '2' | '3' }+ }",
+          "ae := { { ae, '+', ae } | { ae, '*', ae } | n | { '(', ae, ')' } }",
           '',
         ].join('\n'),
         input: '1+2+3',
-        rule: 'rule_expr',
+        rule: 'ae',
+      },
+    },
+    {
+      label: 'direct left recursion',
+      description: 'grammar demonstrating direct left recursion.',
+      parse: {
+        grammar: ["n := { { '1' | '2' | '3' }+ }", "expr := { expr, '+', expr | n }", ''].join('\n'),
+        input: '1+2+3',
+        rule: 'expr',
       },
     },
     {
       label: 'indirect left recursion',
-      description: 'grammar demonstrating indirect left recursion',
+      description: 'grammar demonstrating indirect left recursion.',
+      parse: {
+        grammar: ["n := { { '1' | '2' | '3' }+ }", 'x := { rule_expr }', "expr := { expr, '+', expr | n }", ''].join(
+          '\n',
+        ),
+        input: '1+2+3',
+        rule: 'expr',
+      },
+    },
+    {
+      label: 'structured data',
+      description: 'grammar for a minimal structured data format.',
       parse: {
         grammar: [
-          "rule_num := { { '1' | '2' | '3' }+ }",
-          'rule_x := { rule_expr }',
-          "rule_expr := { rule_x, '+', rule_x | rule_num }",
+          "open := { '{' }",
+          "close := { '}' }",
+          "char := { 'a' | 'b' }",
+          'string := { char+ }',
+          'key := { string }',
+          `value := { '"', string, '"' }`,
+          "object := { key, ':', value }",
+          "objects := { { objects, ',', objects } | object? }",
+          'root := { open, objects, close }',
           '',
         ].join('\n'),
-        input: '1+2+3',
-        rule: 'rule_expr',
+        input: '{a:"b"}',
+        rule: 'root',
       },
     },
   ]
